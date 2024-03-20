01d58bd315b1cfdecd7ab8433f86644ca04ea382
merce5 <mercealomar@gmail.com>
2024-03-20 18:29:20 +0100

    Added EF Core Context and Initial Migration

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,6 +1,18 @@
+using infrastructure.Clients;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Setup DB
+builder.Services.AddDbContext<CsgoDbContext>(op =>
+    op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultValue"))
+);
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
